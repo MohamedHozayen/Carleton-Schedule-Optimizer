a2ee40d1f29d1db6955eb89c0ce5fb330835abbf
Sean <seantohidi@cmail.carleton.ca>
2015-09-21 22:39:35 -0400

    Switched to using classes
    The course data is now stored in a class containing the lectures,
    tutorials, and labs. Each section has its own class containing all the
    section info. The course class has a method to combine all tutorials
    common to a lecture.

---
+++ ScheduleOptimizer.py
@@ -0,0 +1,225 @@
+import sys
+import requests
+import os.path
+import pickle
+from bs4 import BeautifulSoup
+	
+class Course:
+	# This class has attributes containing the course title and the sections for each of the lectures, labs, and tutorials
+	def __init__(self, title, lectures, labs, tutorials):
+		self.title = title
+		self.lectures = lectures
+		self.labs = labs
+		self.tutorials = tutorials
+
+	def ChangeTutorials(self, newtutorials):
+		self.tutorials = newtutorials
+
+	def CombineTutorials(self):
+		sections = '' # This string will hold all the sections for a given lecture (A, B, etc.)
+		firstletter = ''
+		newtutorials = []
+		firstpass = True
+		i = 0 # i counts each tutorial in the list of tutorials
+		x = 0 # x keeps track of the index containing the first tutorial for a given section (A, B, etc.)
+		for tutorial in self.tutorials:
+			currentletter = tutorial.GetSection()[0]
+			# Here we are not at the first tutorial for a given lecture section, so we add the current section to the growing sections string
+			if currentletter == firstletter:
+				sections = sections + '/' + tutorial.GetSection()
+				i = i + 1
+			# Here we are at the first tutorial for a given lecture section, so we assign the first letter. If we are not on the first pass, then we have hit the end of a section's tutorials and must add the previous letter's combined tutorial to the newtutorial list 
+			else:
+				firstletter = currentletter
+				if firstpass == True:
+					firstpass = False
+				else:
+					# Since we are not on the first pass, we must put the combined sections into the first tutorial of a given letter and then add it to the newtutorials list
+					newtutorials.append(self.tutorials[x].ChangeSection(sections))
+					x = i
+					i = i + 1
+				# Here the sections string restarts, beginning with the current tutorial's section
+				sections = tutorial.GetSection()
+		# This step is skipped if there are no tutorials
+		if not self.tutorials == []:
+			# Here we put the combined sections into the first tutorial of the last letter and add it to the newtutorials list. We then change the tutorials in the object to the new combined ones.
+			newtutorials.append(self.tutorials[x].ChangeSection(sections))
+			self.ChangeTutorials(newtutorials)
+		
+class Section:
+	# This class has 4 instance variables containing info on the listings
+	def __init__(self, section, CRN, time, day, location, prof):
+		self.section = section
+		self.CRN = CRN
+		self.time = time
+		self.day = day
+		self.location = location
+		self.prof = prof
+	
+	def GetSection(self):
+		return self.section
+		
+	def ChangeSection(self, newsection):
+		self.section = newsection
+		return self
+
+def getWebsiteData(term, subject, coursecode):
+	url = 'https://central.carleton.ca/prod/bwckschd.p_get_crse_unsec'
+	params = 'term_in='+term+'&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj='+subject+'&sel_crse='+coursecode+'&sel_title=&sel_schd=%25&sel_from_cred=&sel_to_cred=&sel_levl=%25&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a'
+	r = requests.post(url, data=params)
+	soup = BeautifulSoup(r.text, "html.parser", from_encoding='utf8')
+	return soup
+
+def getCourseData(data):
+	# These lists will contain all the sections of the corresponding type for the given course
+	lectures = []
+	labs = []
+	tutorials = []
+	
+	# These strings are replaced when found in the professor list
+	r1 = '(<abbr title="Primary">P</abbr>)</td>'
+	r2 = '<abbr title="To Be Announced">'
+	r3 = '</abbr></td>'
+	
+	# Here we make a list containing all the sections and all their details
+	lis = []
+	for child in data.children:
+		for item in child:
+			if item != '\n':
+				lis.append(item)
+	
+	# When the flag is true, we are grabbing the section title. When the flag is false, we are grabbing the section details. When the counter is at 2, we have all the data needed to record a sectioon
+	sectionflag = True
+	firstSectionGrab = True
+	counter = 0
+
+	for child in lis:
+		child = str(child)
+		sectionflag = not sectionflag
+		
+		# Here we gather the section letter and CRN for each section
+		if sectionflag:
+			crnAndSection = child[114:].strip('</a></th>').partition(' - ')[2]
+			courseCRN = crnAndSection.partition(' - ')[0]
+			courseSection = crnAndSection.partition(' - ')[2].partition(' - ')[2]
+			
+			# On the first pass we grab the course title
+			if firstSectionGrab:
+				courseTitle = child[114:].strip('</a></th>').partition(' - ')[0]
+				firstSectionGrab = not firstSectionGrab
+			else:
+				counter = counter+1
+			
+		# Here we gather the details for the current section
+		else:	
+			j = 1
+			typeIncrement = 0
+			for thing in child.splitlines():
+				if j == 11 :
+					courseType = thing.replace('Schedule Type','').replace('Campus','').replace('Main','Lecture').strip('  ')
+					# Here we check if we are dealing with a lecture or not (shown as 'Main')
+					if 'Main' in thing:
+						typeIncrement = 2
+					else:
+						typeIncrement = 0
+				# The increment depends on the type of section we are looking at and is determined earlier
+				if j == (28 + typeIncrement):
+					courseTime = thing[22:].strip('</td>').replace('am','').replace('pm','').replace(' ','')
+				if j == (29 + typeIncrement):
+					courseDay = thing[22:].strip('</td>')
+				if j == (30 + typeIncrement):
+					courseLocation = thing[22:].strip('</td>')
+				if j == (33 + typeIncrement):
+					courseProf = thing[22:].replace(r1,'').replace(r2,'').replace(r3,'').replace('   ',' ').replace('  ',' ')
+				j += 1	
+			counter = counter+1
+
+		# Here we make an instance of the section class and add it to the corresponding list			
+		if counter == 2: 
+			section = Section(courseSection,courseCRN,courseTime,courseDay,courseLocation,courseProf)
+			if any(i.isdigit() for i in courseSection): # Lecture sections don't have numbers
+				if courseType == 'Laboratory':
+					labs.append(section)
+				else: # Here the section is a tutorial
+					tutorials.append(section)
+			else: # Here the section is a lecture
+				lectures.append(section)
+			counter = 0 # Reset the counter
+			
+	course = Course(courseTitle, lectures, labs, tutorials)
+	return course
+
+def getSemesterData(term, subject, coursecode):
+	semesterData = []
+	
+	# We add each course's data to the semester data
+	for i in range(1, len(subject)+1):
+		soup = getWebsiteData(term, subject[str(i)], coursecode[str(i)])
+		tag = soup.body.contents[5].contents[13]
+		course = getCourseData(tag)
+		semesterData.append(course)
+	return semesterData
+	
+def outputAllSectionData(semesterData):
+	# We open an output text file
+	text_file = open("Output.txt", "w")
+
+	for course in semesterData:
+		text_file.write('********************************************\n')
+		text_file.write(course.title+'\n\n')
+		
+		if not course.lectures == []:
+			text_file.write('Lectures\n-------------------\n')
+			for lecture in course.lectures:
+				text_file.write('Section: '+lecture.section+'\n')
+				text_file.write('CRN: '+lecture.CRN+'\n')
+				text_file.write('Time: '+lecture.time+'\n')
+				text_file.write('Days: '+lecture.day+'\n')
+				text_file.write('Location: '+lecture.location+'\n')
+				text_file.write('Professor: '+lecture.prof+'\n\n')
+		if not course.labs == []:
+			text_file.write('Labs\n-------------------\n')
+			for lab in course.labs:
+				text_file.write('Section: '+lab.section+'\n')
+				text_file.write('CRN: '+lab.CRN+'\n')
+				text_file.write('Time: '+lab.time+'\n')
+				text_file.write('Days: '+lab.day+'\n')
+				text_file.write('Location: '+lab.location+'\n')
+				text_file.write('Professor: '+lab.prof+'\n\n')
+		if not course.tutorials == []:
+			text_file.write('Tutorials\n-------------------\n')
+			for tutorial in course.tutorials:
+				text_file.write('Section: '+tutorial.section+'\n')
+				text_file.write('CRN: '+tutorial.CRN+'\n')
+				text_file.write('Time: '+tutorial.time+'\n')
+				text_file.write('Days: '+tutorial.day+'\n')
+				text_file.write('Location: '+tutorial.location+'\n')
+				text_file.write('Professor: '+tutorial.prof+'\n\n')
+			
+	text_file.close()
+
+def getSemesterObject():
+	# Check if the dump is present. If so, open the dump to get the data. If not we must produce the data and then dump it.
+	if not os.path.isfile('data.dump'):
+		# These variables determine the courses we use for the schedule
+		term = '201530'
+		subject = {'1': 'SYSC', '2': 'SYSC', '3': 'MATH', '4': 'ELEC', '5': 'CCDP', }
+		coursecode = {'1': '2004', '2': '2001', '3': '2004', '4': '2501', '5': '2100', }
+
+		semesterData = getSemesterData(term, subject, coursecode)
+		outputAllSectionData(semesterData)
+		# Here we dump the data
+		with open('data.dump', 'wb') as output:
+			pickle.dump(semesterData, output, pickle.HIGHEST_PROTOCOL)
+	else:
+		# Here we just load the data 
+		with open('data.dump', 'rb') as input:
+			semesterData = pickle.load(input) # protocol version is auto detected	
+	return semesterData
+
+def main():
+	semesterData = getSemesterObject()
+	for course in semesterData:
+		course.CombineTutorials()
+	outputAllSectionData(semesterData)
+main()
