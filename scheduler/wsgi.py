106fdc93bf2b5e38413712667ba64cfa41b58f72
seanrt <seanrtohidi@gmail.com>
2016-01-18 21:45:52 -0500

    Another one

--- scheduler/wsgi.py
+++ scheduler/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for scheduleoptimizer project.
+WSGI config for scheduler project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -7,10 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scheduler.settings")
+
+# application = get_wsgi_application()
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scheduler.settings")
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
