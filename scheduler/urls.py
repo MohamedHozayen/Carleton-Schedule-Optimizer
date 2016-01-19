3b20bdfbddefc8875e4c378d2a3b369915b4e9d2
seanrt <seanrtohidi@gmail.com>
2016-01-18 22:33:53 -0500

    Another one

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.scheduler, name='index'),
 ]
+
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
