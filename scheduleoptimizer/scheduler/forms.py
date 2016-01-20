fe8fb46e712a9a28058f3a1c9a461c67675b9374
seanrt <seanrtohidi@gmail.com>
2016-01-20 18:14:21 -0500

    Added a default submission form

--- scheduleoptimizer/scheduler/forms.py
+++ scheduleoptimizer/scheduler/forms.py
@@ -8,8 +8,14 @@ class ScheduleForm(forms.Form):
 	c3 = forms.CharField(label='Course 3', max_length=8, required = False)
 	c4 = forms.CharField(label='Course 4', max_length=8, required = False)
 	c5 = forms.CharField(label='Course 5', max_length=8, required = False)
-	c6 = forms.CharField(label='Course 6', max_length=8, required = False)
 	def is_valid(self):
 		# Run the parent validation so django won't complain about not checking
 		valid = super(ScheduleForm, self).is_valid()
 		return True
+	def default(self):
+		self.initial['semester'] = '201610'
+		self.initial['c1'] = 'SYSC2003'
+		self.initial['c2'] = 'SYSC2100'
+		self.initial['c3'] = 'ELEC2607'
+		self.initial['c4'] = 'COMP1805'
+		self.initial['c5'] = 'STAT3502'
