66ce4c15405762b1345ca16a738c433f4bd84910
seanrt <seanrtohidi@gmail.com>
2016-06-12 20:46:40 +0300

    Padded the start and end times of sections in the calendar

--- scheduler/static/scheduler/calendarcode.js
+++ scheduler/static/scheduler/calendarcode.js
@@ -34,13 +34,15 @@ function addCalanderEvent(title, start, end, days, colour) {
 		return eventObject;
 }
 
-// 5 minutes are added to the end time of a section. The end time always ends in 5
-function addFiveMinutes(time) {
-    // if (time.substring(3,5) == '25') {
-    //   time = time.substring(0,3)+'30';
-    // }
-    // else { // Here the end time is XX:55
-    //   time = time.
-    // }
-    return time;
-}
+// // 5 minutes are added to the end time of a section. The end time always ends in 5
+// function addFiveMinutes(time) {
+//     if (time.substring(3,5) == '25') {
+//       time = time.substring(0,3)+'30';
+//     }
+//     else { // Here the end time is XX:55
+//       time = time.
+//     }
+//     return time;
+//     // var value = '16:00';
+//     return '17:00';
+// }
