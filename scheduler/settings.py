966a02002d3f5fe56d916721990741181f281126
Sean <seanrtohidi@gmail.com>
2016-10-25 16:47:49 -0400

    Removed the duplicated static files

--- scheduler/settings.py
+++ scheduler/settings.py
@@ -124,8 +124,12 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
+)
+
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
